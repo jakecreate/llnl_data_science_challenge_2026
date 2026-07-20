4f52fe38b760c14ab95f7164e1998ee240d44d53
charocon <ocon.char22@gmail.com>
2026-07-20 14:43:11 -0700

    Task 1 & 2 completed

--- src/mcp_server.py
+++ src/mcp_server.py
@@ -1,4 +1,7 @@
+import numpy as np
 from fastmcp import FastMCP
+from skimage.filters import threshold_otsu, try_all_threshold
+from PIL import Image
 
 # Initialize the MCP server
 mcp = FastMCP("CT Segmentation")
@@ -16,7 +19,30 @@ def segment_ct_dataset(input_filepath: str, output_filepath: str, threshold: flo
     Returns:
         A status message indicating success and the save location, or an error message.
     """
-    pass # Implementation goes here
+
+    # Raw CT scan data turns into .npy file 
+
+    try:
+        # A simulated CT scan: A 3D Numpy array (.npy) where values correspond to X-ray CT density within [0, 1].
+        
+        # Segmentation based on the color density values in the .npy file, the saved file should be binary 0/1 as a mask
+        # the same size/dimesions as the original .npy file
+
+        file_to_segment = np.load(input_filepath)
+
+        thresh = threshold_otsu(file_to_segment)
+        mask = (file_to_segment > thresh).astype(np.uint8)  
+
+        np.save(output_filepath, mask)
+
+    except FileNotFoundError:
+        return f"Segmentation failed: input file not found: {input_filepath}"
+    else:
+        return (
+            f"Segmentation successful. Saved to: {output_filepath}. "
+            f"Otsu threshold: {float(thresh):.10g}."
+        )
+
 
 @mcp.tool()
 def visualize_slice(input_filepath: str, output_filepath: str, slice_index: int, axis: int = 0) -> str:
@@ -32,8 +58,24 @@ def visualize_slice(input_filepath: str, output_filepath: str, slice_index: int,
     Returns:
         A status message indicating success and the save location, or an error message.
     """
-    pass # Implementation goes here
+    
+    try:
+        file_to_visualize = np.load(input_filepath)
 
+        specified_slice = np.take(file_to_visualize, slice_index, axis=axis)
+
+        s = specified_slice.astype(np.float32)
+        s = (s - s.min()) / (s.max() - s.min()) * 255
+        visualized_slice = Image.fromarray(s.astype(np.uint8), mode='L')
+
+        visualized_slice.save(output_filepath)
+    
+    except FileNotFoundError:
+        return f"Visualization failed: input file not found: {input_filepath}"
+
+    else:
+        return (f"Visualization successful. Saved to: {output_filepath}. ")
+    
 @mcp.tool()
 def skeletonize(input_filepath: str, output_filepath: str) -> str:
     """
