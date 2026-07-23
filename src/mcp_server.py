2b537cbb3f73066ff48abd229f6896caa5cba321
Brandon Del Mundo <brandon.delmundo@gmail.com>
2026-07-23 16:36:42 -0700

    tool to create a box around defects'

--- src/mcp_server.py
+++ src/mcp_server.py
@@ -85,13 +85,12 @@ def skeletonize(input_filepath: str, output_filepath: str) -> str:
 def pyvista_screenshot(input_filepath: str, output_filepath: str, slice: int):
     """
     Take a screenshot of an .stl file for evaluation of defects.
-
     Args:
         input_filepath: Path to the .stl file in missing_struts
-        output_filepath: Path to save the screenshot (Should point to the screenshots directory and be in a .png format)
+        output_filepath: Path to save the screenshot
         slice: The index of the
     Returns:
-        A .png image in the screenshots directory representing a slice on the z_axis at the point of the slice
+        A status message indicating success and the save location, or an error message
     """
     try:
         mesh = load(input_filepath)
@@ -107,15 +106,34 @@ def bitwise_xor_skeletion(input_filepath_1: str, input_filepath_2: str, output_f
     Args:
         input_filepath_1: Path to the skeletonized image file 1
         input_filepath_2: Path to the skeletonized image file 2
-        output_filepath: Path to save the screenshot (Should point to the slices directory and be in a .png format)
+        output_filepath: Path to save the screenshot
     Returns:
-        A .pngimage of the bitwise xor of both input images
+        A status message indicating success and the save location, or an error message
     """
     try:
         bitwise_xor(input_filepath_1, input_filepath_2, output_filepath)
         return f"Successfully saved bitwise xor to {output_filepath}"
     except Exception as e:
         return f"Error while running bitwise_xor_skeletion: {e}"
+
+@mcp.tool()
+def create_image_rectangle(input_filepath: str, output_filepath: str, top_left_point: tuple, bottom_right_point: tuple):
+    """
+    Takes in an image file and two points (x, y) which indicates the top left and bottom right of the rectangle.
+    This creates a rectangle on the image.
+    Args:
+        input_filepath: Path to the skeletonized image file
+        output_filepath: Path to the outputted image file
+        top_left_point: A tuple (x, y) corresponding to the top left conrer of the rectangle
+        bottom_right_point: A tuple (x, y) corresponding to the bottom right conrer of the rectangle
+    Returns:
+        An image with the rectangle box added to it.
+    """
+    try:
+        create_rectangle(input_filepath, output_filepath, top_left_point, bottom_right_point)
+        return f"Successfully saved image to {output_filepath}"
+    except Exception as e:
+        return f"Error while running create_image_rectangle: {e}"
 
 if __name__ == "__main__":
     # Run the FastMCP server, exposing the tools over standard I/O (default)
--- src/tool_opencv.py
+++ src/tool_opencv.py
@@ -11,3 +11,9 @@ def bitwise_xor(input_filepath_1: str, input_filepath_2: str, output_filepath: s
     except Exception as e:
         print(f"Error while doing a bitwise xor: {e}")
         return False
+
+def create_rectangle(input_filepath: str, output_filepath: str, top_left_point: tuple, bottom_right_point: tuple):
+    image = cv2.imread(input_filepath)
+    cv2.rectangle(image, top_left_point, bottom_right_point, (0, 0, 255), 2)
+    cv2.imwrite(output_filepath, image)
+    return True
